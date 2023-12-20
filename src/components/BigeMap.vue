e490449eaff454e29dd30a8d87438c3783a60484
huangzhizhen <huangzhizhen@comtom.cn>
2023-12-20 11:19:58 +0800

    feat: 创建项目

---
+++ src/components/BigeMap.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="bige-map">
+    bigeMap
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style lang="less" scoped></style>
