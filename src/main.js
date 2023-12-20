96cc0a078004fedb6e5196ccfae41f9f4ebd6d06
huangzhizhen <huangzhizhen@comtom.cn>
2023-12-20 17:45:31 +0800

    feat: 引入antd

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Antd from 'ant-design-vue'
+import 'ant-design-vue/dist/antd.css';
 import './style/common.less'
 
 Vue.config.productionTip = false
+Vue.use(Antd)
 
 new Vue({
   render: h => h(App),
